23d33fbbf66f81d2632d3a748de599b2ca7afeec
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2021-06-26 02:34:58 +0100

    update interface

--- src/RVMFusion.jl
+++ src/RVMFusion.jl
@@ -34,6 +34,30 @@ end
 
 function RVM(
     XH::Matrix{T}, XL::Matrix{T}, t::Vector{Int64},
+    α_init::Float64=1.0, β_init::Float64=1.0;
+    kw...
+) where T<:Real
+    n = size(XH, 1)
+    d = size(XH, 2)
+    size(t, 1) == n || throw(DimensionMismatch("Sizes of X and t mismatch."))
+
+    K = size(unique(t), 1)
+    if K > 2
+        α = ones(T, d, K) .* α_init
+        β = ones(T, d, K) .* β_init
+        t2 = [j == t[i] for i ∈ 1:n, j ∈ 1:K]
+        RVM!(XH, XL, convert(Matrix{T}, t2), α, β; kw...)
+    elseif K == 2
+        α = ones(T, d) .* α_init
+        β = ones(T, d) .* β_init
+        RVM!(XH, XL, convert(Vector{T}, t), α, β; kw...)
+    else
+        throw(TypeError("Number of classes less than 2."))
+    end
+end
+
+function RVM(
+    XH::Matrix{T}, XL::Matrix{T}, t::Vector{Int64},
     XLtest::Matrix{T}, α_init::Float64=1.0,
     β_init::Float64=1.0; kw...
 ) where T<:Real
