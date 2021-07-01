70ccee091d9dec9d52543088e6bcd9fd5546ec9c
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2021-07-01 22:34:41 +0100

    change default model type from array to union

--- src/RVMFusion.jl
+++ src/RVMFusion.jl
@@ -33,7 +33,7 @@ function RVM(X::Matrix{T}, t::Vector{Int}, α_init::T=convert(T, 1.0);
 end
 
 function RVM(
-    XH::Matrix{T}, XL::Matrix{T}, t::Vector{Int},
+    XH::Union{Matrix{T}, BnRVModel{T}}, XL::Matrix{T}, t::Vector{Int},
     α_init::T=convert(T, 1.0), β_init::T=convert(T, 1.0);
     kw...
 ) where T<:Real
