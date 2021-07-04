1bfd08b3d4b1734e057a94d7b1e79b29b63c92ee
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2021-07-05 00:14:59 +0100

    change step size

--- src/binomial.jl
+++ src/binomial.jl
@@ -327,7 +327,7 @@ function cal_rotation(
     g, gp = (zeros(T, d, d) for _ = 1:2)
     #@debug "U" U' * U size(U)
     #@debug "g" size(g)
-    η = [1f-5]
+    η = [1f-6]
     a, y = (Vector{T}(undef, n) for _ = 1:2)
     mul!(a, X, U' * wh)
     @debug "a" a
