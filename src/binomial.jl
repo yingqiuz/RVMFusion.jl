2f4bf37cfa7e4b07cd588ffd9fdac5721271536a
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2021-07-05 19:44:51 +0100

    increase step size

--- src/binomial.jl
+++ src/binomial.jl
@@ -333,8 +333,8 @@ function cal_rotation(
     mul!(a, X, U' * wh)
     y = logistic.(a)
     llhp = Inf
-    bs = 1000
-    η = [1f-7]
+    bs = n
+    η = [1f-6]
     for iter = 2:maxiter
         for nn = 1:Int(round((n/bs)))
             @views mul!(
