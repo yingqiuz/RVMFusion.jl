244b7e93e146b574ad6432d86aa9a0f0496d795f
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2021-07-05 11:22:38 +0100

    change initial step size

--- src/binomial.jl
+++ src/binomial.jl
@@ -338,7 +338,7 @@ function cal_rotation(
     @debug "(y .- t)' * X" (y .- t)' * X
     llhp = Inf
     bs = 10
-    η = [1f-4]
+    η = [1f-6]
     for iter = 2:maxiter
         for nn = 1:Int(round((n/bs)))
             @views mul!(
