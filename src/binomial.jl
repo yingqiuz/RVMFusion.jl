60674ee90054790d55d8b01e6f236f9d57d7d2b5
Ying-Qiu Zheng <yingqiuz@users.noreply.github.com>
2021-07-05 01:24:41 +0100

    increase initial stepsize

--- src/binomial.jl
+++ src/binomial.jl
@@ -327,7 +327,7 @@ function cal_rotation(
     g, gp = (zeros(T, d, d) for _ = 1:2)
     #@debug "U" U' * U size(U)
     #@debug "g" size(g)
-    η = [1f-5]
+    η = [1f-4]
     #β = [0.9f0]
     a, y = (Vector{T}(undef, n) for _ = 1:2)
     mul!(a, X, U' * wh)
@@ -354,7 +354,7 @@ function cal_rotation(
         llh = sum(log1pexp.((1 .- 2 .* t) .* a))
         @debug "llh" llh
         copyto!(gp, g)
-        while !(llh - llhp < 0)
+        while !(llh - llhp <= 0)
             η ./= 2
             U .= Up .- g .* η
             mul!(a, X, U' * wh)
